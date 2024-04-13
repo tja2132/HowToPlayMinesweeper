efda4f37fce0b7ae3fd80a2646ffdde999d93700
tja2132 <tja2132@columbia.edu>
2024-04-13 10:45:10 -0400

    add visit log

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template, redirect, url_for
 from flask import Response, request, jsonify
+from datetime import datetime
 import json
 app = Flask(__name__)
 
@@ -9,39 +10,40 @@ lessons = open('./static/lessons.json', encoding="utf8")
 data = json.load(lessons)
 lessons.close()
 
+def get_data_by_id(id):
+    global data
+    for d in data:
+        if str(d['id']) == id:
+            return d
+    return "That ID does not exist"
+
+def log_visit(route_name):
+    with open('visit_log.txt', 'a') as log:
+        log.write(f"{route_name} visited at {datetime.now()}\n")
+
 # ROUTES
 @app.route('/')
 def render_home():
+   log_visit('Home')
    return render_template('home.html')   
 
 @app.route('/start')
 def start_lesson():
-   return render_template('home.html')   
+   log_visit('Start')
+   return redirect(url_for('render_lesson', id=1))
+
 
 @app.route('/learn/<id>', methods=['GET', 'POST'])
 def render_lesson(id):
-    global data
-    r = "That ID does not exist"
-    for d in data:
-        if str(d['id']) == id:
-            r = d
-            print('rendering', r)
-            break
-
-    return render_template('learn.html', result=r, id=id)  
+    result = get_data_by_id(id)
+    log_visit(f'Learn {id}')
+    return render_template('learn.html', result=result, id=id)  
 
 @app.route('/quiz/<id>', methods=['GET', 'POST'])
 def render_quiz(id):
-    global data
-    r = "That ID does not exist"
-    for d in data:
-        if str(d['id']) == id:
-            r = d
-            break
-
-    return render_template('quiz.html', result=r, id=id)  
-
-
+    result = get_data_by_id(id)
+    log_visit(f'Quiz {id}')
+    return render_template('quiz.html', result=result, id=id)  
 
 if __name__ == '__main__':
    app.run(debug = True)
