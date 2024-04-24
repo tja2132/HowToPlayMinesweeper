5f9727bddfd4574d4f4781f9b7327831d47ecd20
tja2132 <tja2132@columbia.edu>
2024-04-24 17:22:55 -0400

    fix quiz progression

--- static/mines.js
+++ static/mines.js
@@ -1,9 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
+
   const grid = document.querySelector('.grid');
-  const flagsLeft = document.querySelector('#flags-left');
-  const result = document.querySelector('#result');
-  const timer = document.querySelector('#timer');
-  const emojiBtn = document.querySelector('.emoji-btn');
   const spreadTimeout = 15;
   let bombPositions = []; // Array of indices where bombs are located
   let width = 10; // Default width
@@ -24,8 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
   
   // Create Board
   function createBoard() {
-      flagsLeft.innerHTML = bombAmount;
+
       grid.innerHTML = ''; // Clear previous grid
+        //   grid.attr('innerHTML', 'test');
       squares = []; // Reset squares
 
       // Create a new div for each square in the game board
@@ -33,7 +30,8 @@ document.addEventListener('DOMContentLoaded', () => {
           const square = document.createElement('div');
           square.setAttribute('id', i);
           square.classList.add(bombPositions.includes(i) ? 'bomb' : 'valid');
-          grid.appendChild(square);
+          console.log('appending square', square)
+          grid.append(square);
           squares.push(square);
 
           // Normal Click
@@ -67,9 +65,109 @@ document.addEventListener('DOMContentLoaded', () => {
               squares[i].setAttribute('data', total);
           }
       }
+
+      console.log(grid)
   }
 
-  // Assuming initGame is called with parameters like:
-  // initGame([1, 25, 49], 10, 10); // Bomb positions, width, height
-});
 
+   // Click on square actions
+   function click(square) {
+    let currentId = square.id;
+    if (isGameOver) { return };
+    if (square.classList.contains('flag') || square.classList.contains('checked')) { return };
+    if (square.classList.contains('one') || square.classList.contains('two') || square.classList.contains('three') || square.classList.contains('four')) {
+      //chording here
+      //check adjacent bomb is flagged and click surrounding squares
+      return
+    }
+    if (square.classList.contains('bomb')) {
+      gameOver();
+    } else {
+      let total = square.getAttribute('data');
+      if (total != 0) {
+        square.classList.add('checked');
+        if (total == 1) { square.classList.add('one') }; // adding classes for colors
+        if (total == 2) { square.classList.add('two') };
+        if (total == 3) { square.classList.add('three') };
+        if (total == 4) { square.classList.add('four') };
+        square.innerHTML = total;
+        return
+      }
+      checkSquare(square, currentId);
+    }
+    square.classList.add('checked');
+  }
+
+  // Check neighboring squares once square is clicked
+  // Creates the spreading effect
+  function checkSquare(square, currentId) {
+    const isLeftEdge = (currentId % width === 0)
+    const isRightEdge = (currentId % width === width -1)
+
+    setTimeout(() => {
+      if (currentId > 0 && !isLeftEdge) {
+        const newId = squares[parseInt(currentId) - 1].id;
+        //const newId = parseInt(currentId) - 1   ....refactor
+        const newSquare = document.getElementById(newId);
+        if (!newSquare.classList.contains('bomb')) {
+          click(newSquare);
+        }
+      }
+      if (currentId > (width-1) && !isRightEdge) {
+        const newId = squares[parseInt(currentId) + 1 - width].id;
+        //const newId = parseInt(currentId) +1 -width   ....refactor
+        const newSquare = document.getElementById(newId);
+        if (!newSquare.classList.contains('bomb')) {
+          click(newSquare);
+        }
+      }
+      if (currentId >= width) {
+        const newId = squares[parseInt(currentId - width)].id;
+        //const newId = parseInt(currentId) -width   ....refactor
+        const newSquare = document.getElementById(newId);
+        if (!newSquare.classList.contains('bomb')) {
+          click(newSquare);
+        }
+      }
+      if (currentId >= (width+1) && !isLeftEdge) {
+        const newId = squares[parseInt(currentId) - 1 - width].id;
+        //const newId = parseInt(currentId) -1 -width   ....refactor
+        const newSquare = document.getElementById(newId);
+        if (!newSquare.classList.contains('bomb')) {
+          click(newSquare);
+        }
+      }
+      if (currentId <= (width*width-2) && !isRightEdge) {
+        const newId = squares[parseInt(currentId) + 1].id;
+        //const newId = parseInt(currentId) +1   ....refactor
+        const newSquare = document.getElementById(newId);
+        if (!newSquare.classList.contains('bomb')) {
+          click(newSquare);
+        }
+      }
+      if (currentId < (width*width-width) && !isLeftEdge) {
+        const newId = squares[parseInt(currentId) - 1 + width].id;
+        //const newId = parseInt(currentId) -1 +width   ....refactor
+        const newSquare = document.getElementById(newId);
+        if (!newSquare.classList.contains('bomb')) {
+          click(newSquare);
+        }
+      }
+      if (currentId <= (width*width-width-2) && !isRightEdge) {
+        const newId = squares[parseInt(currentId) + 1 + width].id;
+        //const newId = parseInt(currentId) +1 +width   ....refactor
+        const newSquare = document.getElementById(newId);
+        if (!newSquare.classList.contains('bomb')) {
+          click(newSquare);
+        }
+      }
+      if (currentId <= (width*width-width-1)) {
+        const newId = squares[parseInt(currentId) + width].id;
+        //const newId = parseInt(currentId) +width   ....refactor
+        const newSquare = document.getElementById(newId);
+        if (!newSquare.classList.contains('bomb')) {
+          click(newSquare);
+        }
+      }
+    }, spreadTimeout);
+  }
